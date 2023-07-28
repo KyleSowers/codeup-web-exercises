518bdc6f6a1580ec192ff412ecf433eaab3d6fcb
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-07-28 13:39:15 -0500

    Added additional changes to work based on personal preferences/additonal cleanup

--- js/external.js
+++ js/external.js
@@ -5,28 +5,40 @@ console.log("Hello from extrenal JavaScript");
 
 alert("Welcome to my Website!");
 
+
 // prompt user for favorite color
 var userFavoriteColor = prompt("Tell me your favorite color.");
 
-// Make sure use of prompt in below alert is capitalized because it begins a sentence
+console.log("Users favorite color is " + userFavoriteColor.charAt(0).toLowerCase() + userFavoriteColor.slice(1) + ".");
+
+// Making user input capitalized because it begins the next alert statement
 userFavoriteColor = userFavoriteColor.charAt(0).toUpperCase() + userFavoriteColor.slice(1);
 
 alert(userFavoriteColor + " is also my favorite color!");
 // End of color prompt/alert
 
+
 /* Below is the first problem of exercise 3 regarding movie rental costs */
 alert('Movie rental rate is $3 a day.')
 
-var theLittleMermaid = prompt("For how many days have you rented out movie: The Little Mermaid?")
+var theLittleMermaid = prompt("For how many days have you rented out the movie: The Little Mermaid?")
 theLittleMermaid = parseInt(theLittleMermaid);
 
-var brotherBear = prompt("For how many days have you rented out movie: Brother Bear?")
+console.log("Days user has had The Little Mermaid: " + theLittleMermaid);
+
+var brotherBear = prompt("For how many days have you rented out the movie: Brother Bear?")
 brotherBear = parseInt(brotherBear);
 
-var hercules = prompt("For how many days have you rented out movie: Hercules?")
+console.log("Days user has had Brother Bear: " + brotherBear);
+
+var hercules = prompt("For how many days have you rented out the movie: Hercules?")
 hercules = parseInt(hercules);
 
+console.log("Days user has had Hercules: " + hercules);
+
 var totalUserCost = (theLittleMermaid + brotherBear + hercules) * 3;
+
+console.log("User cost for all movies currently is: $" + totalUserCost);
 
 alert("Your current rental cost for The Little Mermaid, Brother Bear, and Hercules is $" + totalUserCost);
 
@@ -34,28 +46,41 @@ alert("Your current rental cost for The Little Mermaid, Brother Bear, and Hercul
 alert('Contracting for three FAANG companies is a big deal! Let\'s figure out your payment for the week.');
 
 // prompt answers inputs will be parsed to integers
-var payGoogleContract = prompt("First, how much does the Goolge contract pay per hour?");
+var payGoogleContract = prompt("First, how much does the Google contract pay per hour?");
 payGoogleContract = parseInt(payGoogleContract);
+
+console.log("Google pays: $" + payGoogleContract + " per hour.");
 
 var hoursThisWeekGoogle = prompt("How many hours were worked for Google this week?");
 hoursThisWeekGoogle = parseInt(hoursThisWeekGoogle);
 
+console.log("User hours worked for Google: " + hoursThisWeekGoogle);
+
 var payFacebookContract = prompt("Now, how much does the Facebook contract pay per hour?");
 payFacebookContract = parseInt(payFacebookContract);
+
+console.log("Facebook pays: $" + payFacebookContract + " per hour.");
 
 var hoursThisWeekFacebook = prompt("And how many hours were worked for Facebook this week?");
 hoursThisWeekFacebook = parseInt(hoursThisWeekFacebook);
 
+console.log("User hours worked for Facebook: " + hoursThisWeekFacebook);
+
 var payAmazonContract = prompt("Finally, how much does the Amazon contract pay per hour?");
 payAmazonContract = parseInt(payAmazonContract);
+
+console.log("Amazon pays: $" + payAmazonContract + " per hour.");
 
 var hoursThisWeekAmazon = prompt(" And how many hours were worked for Amazon this week?");
 hoursThisWeekAmazon = parseInt(hoursThisWeekAmazon);
 
+console.log("User hours worked for Amazon: " + hoursThisWeekAmazon);
+
 var totalContractorWeekPay = (payGoogleContract * hoursThisWeekGoogle) + (payFacebookContract * hoursThisWeekFacebook) + (payAmazonContract * hoursThisWeekAmazon);
 
-/* This alert tells user their total pay form above collected prompt input data values */
-alert("Your total pay for the week, by reported pay rate and hours worked, should be $" + totalContractorWeekPay + ".");
+console.log("Total pay for work: $" + totalContractorWeekPay);
+
+alert("Your total pay for the week before taxes should be $" + totalContractorWeekPay + ".");
 //end of contractor weekly pay problem set
 
 /* Below is the third problem from exercise 3 regarding whether an individual can enroll into a class */
@@ -63,9 +88,15 @@ alert("So I hear you want to join a class. Let's find out if you can at this tim
 
 var seatAvailable =  confirm("You are required to verify that the class is not full. Tell us, is space still available to join the class? Selecting \"okay\" advises yes. Selecting \"Cancel\" advises no.");
 
+console.log("User suggests class availability: " + seatAvailable);
+
 var noConflictingSchedule = confirm("You also must have the availability within your schedule to attend the class per class schedule. Have you identified the schedule requirements AND does your availability meet the class schedule requirements? Answer \"okay\" to indicate you DO have availability meeting class schedule. Answer \"cancel\" to indicate your availability DOES NOT meet class schedule needs.");
 
+console.log("User suggests schedule availability works: " + noConflictingSchedule);
+
 var meetsAcceptanceRequirement = seatAvailable && noConflictingSchedule;
+
+console.log("User meets class entry requirements: " + meetsAcceptanceRequirement);
 
 alert("Your current position of acceptance is " + meetsAcceptanceRequirement + ".");
 // End of class acceptance problem set
@@ -73,15 +104,23 @@ alert("Your current position of acceptance is " + meetsAcceptanceRequirement + "
 /* Below is the fourth problem of exercise 3 regarding product offer */
 alert('Does our product offer still apply? Let\'s find out!');
 
-var isPremiumMember = confirm("To begin, premium members qualify for offer, if not expired. Are you a premium member? Answer \"okay\" for yes. Answer \"cancel\" for no.");
+var isPremiumMember = confirm("To begin, being a premium member qualifies for offer. Are you a premium member? Answer \"okay\" for yes or \"cancel\" for no.");
 
-var confirmPurchaseMin = confirm("If you advised in previous prompt you are a premium member, you may submit either answer to move forword now. Otherwise, non members do need to meet a min purchase of 2 items to qualify for product offer. If you are a non premium member, identified by selecting \"cancel\" in last question pop-up, have you purchased a minimum of 2 items?");
+console.log("User is a premium member: " + isPremiumMember);
 
-var offerNotExpired = confirm("Lastly, the offer date must not be expired. Looking at your offer, is the offer date still valid? (meaning the expiry date has not passed) Answer \"okay'\" if offer date is still valid. Answer \"cancel\" is offer date has already passed.");
+var confirmPurchaseMin = confirm("If you answered \"okay\" to being a premium member previously, you may submit either answer now and move forward. Otherwise, non premium members do need to meet the min purchase of 2 items to qualify for product offer. If you are a non premium member, have you purchased a minimum of 2 items? Selecting \"okay\" for yes or \"cancel\" for no");
+
+console.log("User meets min purchase requirement: " + confirmPurchaseMin);
+
+var offerNotExpired = confirm("Lastly, the offer date must not be expired. Looking at your offer, is the offer date still valid? (meaning the expiry date has not passed) Answer \"okay'\" for not expired or \"cancel\" for expired.");
+
+console.log("User offer is not expired: " + offerNotExpired);
 
 var offerValidity = (isPremiumMember || confirmPurchaseMin) && offerNotExpired;
 
-alert("Your offer is accepted: " + offerValidity + ".");
+console.log("User offer was accepted: " + offerValidity);
+
+alert("Your offer will be accepted: " + offerValidity + ".");
 // End of product offer problem set
 
 
