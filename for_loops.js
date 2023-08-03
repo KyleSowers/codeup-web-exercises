e31b6b8fdd90c31abdcf4e7289d1de139fa36c15
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-08-03 15:30:49 -0500

    Completed first problem in for_loop exercise

---
+++ for_loops.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let baseNumber = 7
+function showMultiplicationTable() {
+    for (let x = 1; x <= 10 ; x++) {
+        console.log(baseNumber + " x " + x + " = "(baseNumer * x));
+    }
+}
