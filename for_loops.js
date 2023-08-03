1bcf8ecd241e434a2259f0ebbef9b781b0eb57b2
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-08-03 17:45:50 -0500

    Completed problem 4 for_loops exercise

--- for_loops.js
+++ for_loops.js
@@ -10,11 +10,28 @@
 
 
 
-for (let x = 1; x <= 10; x++) {
-    let tenRandomNumbers = Math.floor(Math.random() * (200 - 20 + 1) + 20);
-    if (tenRandomNumbers % 2===0) {
-        console.log(tenRandomNumbers + " is even.");
-    } else {
-        console.log(tenRandomNumbers + " is odd.");
+// for (let x = 1; x <= 10; x++) {
+//     let tenRandomNumbers = Math.floor(Math.random() * (200 - 20 + 1) + 20);
+//     if (tenRandomNumbers % 2===0) {
+//         console.log(tenRandomNumbers + " is even.");
+//     } else {
+//         console.log(tenRandomNumbers + " is odd.");
+//     }
+// }
+
+
+let result = "";
+let numberOfLoops = 9;
+let  currentLoop = 1;
+
+for ( currentLoop; currentLoop <= numberOfLoops; currentLoop++) {
+    let line = "";
+
+    for (let i = 1; i <= currentLoop; i++) {
+        line += currentLoop;
     }
+
+    result += line + "\n";
 }
+
+console.log(result);
