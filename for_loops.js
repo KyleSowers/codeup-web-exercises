5bd45943195841c4b11ccbbc3be61fac0c12fd72
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-08-03 16:19:55 -0500

    Completed problem 3 of for_loops exercise

--- for_loops.js
+++ for_loops.js
@@ -1,9 +1,20 @@
 "use strict";
 
-let userNumber = prompt("Provide a number for multiplication table.");
-userNumber = parseInt(userNumber);
-function showMultiplicationTable() {
-    for (let x = 1; x <= 10 ; x++) {
-        console.log(userNumber + " x " + x + " = " +(userNumber * x));
+// let userNumber = prompt("Provide a number for multiplication table.");
+// userNumber = parseInt(userNumber);
+// function showMultiplicationTable() {
+//     for (let x = 1; x <= 10 ; x++) {
+//         console.log(userNumber + " x " + x + " = " +(userNumber * x));
+//     }
+// } console.log(showMultiplicationTable());
+
+
+
+for (let x = 1; x <= 10; x++) {
+    let tenRandomNumbers = Math.floor(Math.random() * (200 - 20 + 1) + 20);
+    if (tenRandomNumbers % 2===0) {
+        console.log(tenRandomNumbers + " is even.");
+    } else {
+        console.log(tenRandomNumbers + " is odd.");
     }
-} console.log(showMultiplicationTable());
+}
