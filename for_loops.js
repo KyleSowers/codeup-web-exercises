127fb7a3431c7e77a15795b107261d1481fb0374
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-08-07 11:25:09 -0500

    Created JS and HTML file for Array exercise...Also Completed TODO 1

--- for_loops.js
+++ for_loops.js
@@ -32,3 +32,46 @@
 //         continue;
 //     }
 // }
+
+
+//Second Run of Loop Exercises
+//Problem 2 "Show Multiplication Table"
+function showMultiplicationTable(number) {
+    for (let i = 1; i <= 10; i++) {
+        if (typeof number !== "number") {
+            continue;
+        }
+        console.log(number + " x " + i + ' = ' + (number * i));
+    }
+}
+showMultiplicationTable(undefined);
+showMultiplicationTable(null);
+showMultiplicationTable(7);
+showMultiplicationTable("");
+showMultiplicationTable(30);
+showMultiplicationTable("Bob");
+//End of Problem 2 "Show Multiplication Table"
+
+//Problem 3 "For Loop 10 Random Between 20 and 200"
+// let randomNumberGenerated = Math.floor(Math.random() * 181) + 20;
+
+for (let i = 1; i <= 10; i++) {
+    let randomNumberGenerated = Math.floor(Math.random() * 181) + 20;
+    if (randomNumberGenerated % 2 === 0) {
+        console.log(randomNumberGenerated + " is even");
+    } else if (randomNumberGenerated % 2 === 1) {
+        console.log(randomNumberGenerated + " is odd");
+    }
+}
+
+
+var line = "";
+
+for (let initialNumber = 1; initialNumber <= 9; initialNumber++) {
+
+    for (let additionalNumber = 1; additionalNumber <= initialNumber; additionalNumber++) {
+         line = (line + initialNumber);
+    } line += "\n";
+}
+
+console.log(line);
---
+++ while.js
@@ -0,0 +1,14 @@
+"use strict";
+// let i = 2;
+//
+// while (i < 65537) {
+//     console.log(i);
+//     i *= 2;
+// }
+
+// let allCones = Math.floor(Math.random() *50) + 50;
+//
+// do {
+//     let customerPurchase = Math.floor(Math.random() * 5) + 1;
+//
+// }
