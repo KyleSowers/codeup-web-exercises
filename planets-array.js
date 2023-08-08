971f9a01acc250faf85e65ace5e91656253e84ce
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-08-08 09:10:43 -0500

    Just in case

--- planets-array.js
+++ planets-array.js
@@ -24,5 +24,17 @@
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
+    planetsString = planetsArray.join(" <br> ");
+    console.log(planetsString);
 
-})();
+
+    //Bonus
+    planetsArray
+   planetsArray.push("</ul>");
+   planetsArray.unshift("<ul>");
+   console.log(planetsArray);
+
+
+    //"<ul><li>element1</li><li>element2</li>... </ul>
+
+
