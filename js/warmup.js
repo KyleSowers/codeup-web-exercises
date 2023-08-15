e11d80872ab9743473947ca3cf6fb10654d7f981
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-08-15 09:29:23 -0500

    Completed 15 Aug morning warup -KS

---
+++ js/warmup.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// function logUser(string) {
+//     return (string + " has logged in for the day");
+// }
+//
+// console.log(logUser("kyle"));
+
+function logUser(string) {
+    if (typeof string === "string");
+    return (`${string} has logged in for the day`);
+}
+
+console.log(logUser("Kyle"));
