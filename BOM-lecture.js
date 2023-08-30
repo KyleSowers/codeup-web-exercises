ba6add17714cfcdc16882ab7fc6b7d2db8d6ea31
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-08-30 15:18:35 -0500

    difuse the BOM exercise completed

--- BOM-lecture.js
+++ BOM-lecture.js
@@ -1,24 +1,24 @@
 "use strict"
 
 //set interval
-// let count = 0;
-//
-// let max = 10;
-//
-// let interval = 1000;
-//
-// let intervalId= window.setInterval(function () {
-//     if (count >= max) {
-//         clearInterval(intervalId);
-//         console.log("All done");
-//     } else {
-//         count++;
-//         console.log("Repeating this line " + count);
-//     }
-// }, interval);
+let count = 0;
+
+let max = 10;
+
+let interval = 1000;
+
+let intervalId= window.setInterval(function () {
+    if (count >= max) {
+        clearInterval(intervalId);
+        console.log("All done");
+    } else {
+        count++;
+        console.log("Repeating this line " + count);
+    }
+}, interval);
 
 //set timeout
-let delay = 5000 // delay time in milliseconds
+let delay = 10000 // delay time in milliseconds
 
 let timeOutId = setTimeout(function () {
     alert("Here is a delayed hello!");
